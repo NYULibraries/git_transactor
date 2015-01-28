d017ed2235f0ba3d7b7645d8ef9efe943e32f14d
jgpawletko <jgpawletko@gmail.com>
2015-01-28 14:09:39 -0500

    add test repo setup

--- features/step_definitions/git_transactor_steps.rb
+++ features/step_definitions/git_transactor_steps.rb
@@ -1,4 +1,23 @@
-Given(/^that the file does not exist in the repository$/) do
+def nuke_repo(repo)
+  FileUtils.rm_rf(repo) if File.exists?(repo)
+end
+
+def init_repo(repo)
+  FileUtils.mkdir(repo)
+  g = Git.init(repo)
+  dummy_file = "#{repo}/foo.txt"
+  File.open(dummy_file, 'w') {|f| f.puts("#{dummy_file}")}
+  g.add(File.basename(dummy_file))
+  g.commit('Initial commit')
+end
+
+Given(/^that the git repository exists$/) do
+  repo = 'tmp/repo'
+  nuke_repo(repo)
+  init_repo(repo)
+end
+
+Given(/^the file does not exist in the git repository$/) do
   pending # express the regexp above with the code you wish you had
 end
 
