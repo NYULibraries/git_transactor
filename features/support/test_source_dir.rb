fe5a0b82faaa735f201a8044d831e87572161389
jgpawletko <jgpawletko@gmail.com>
2015-01-28 16:01:29 -0500

    clean up TestSourceDir class

--- features/support/test_source_dir.rb
+++ features/support/test_source_dir.rb
@@ -8,10 +8,6 @@ class TestSourceDir
   end
   def init
     FileUtils.mkdir(@path)
-    dummy_file = "#{@path}/foo.txt"
-    File.open(dummy_file, 'w') {|f| f.puts(dummy_file)}
-    g.add(File.basename(dummy_file))
-    g.commit('Initial commit')
   end
   def create_file(fname, msg)
     File.open(File.join(@path, fname), 'w') {|f| f.puts(msg)}
