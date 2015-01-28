9392403ab5b03945f4c6bde806ba79d1a61818ee
jgpawletko <jgpawletko@gmail.com>
2015-01-28 11:15:15 -0500

    add step definitions file

---
+++ features/step_definitions/git_transactor_steps.rb
@@ -0,0 +1,19 @@
+Given(/^that the file does not exist in the repository$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^there is an add\-request for the file$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I process the add\-request$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the file in the repository$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)" in the commit log$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
