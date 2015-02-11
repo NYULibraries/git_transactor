297c624258449f22fcd12cdb6a4baa48d9935332
jgpawletko <jgpawletko@gmail.com>
2015-02-11 17:04:12 -0500

    add QueueEntry exception for multi-line files

---
+++ spec/git_transactor/queue_entry_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+module GitTransactor
+  describe QueueEntry do
+    let(:fixture_root) { File.join('spec','fixtures','queue_entries') }
+    let(:valid_entry)  { File.join(fixture_root, 'valid-entry.csv') }
+    let(:multi_line_entry)  { File.join(fixture_root, 'multi-line-entry.csv') }
+
+    context "when class is instantiated" do
+      subject { QueueEntry.new(valid_entry) }
+      it { is_expected.to be_a(GitTransactor::QueueEntry) }
+    end
+
+    context "when given a multi-line entry file" do
+      it "should raise and ArgumentError" do
+        expect{ QueueEntry.new(multi_line_entry) }.to raise_error(ArgumentError)
+      end
+    end
+  end
+end
