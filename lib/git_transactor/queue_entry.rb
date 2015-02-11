844caf2f75c2a4c7ad4f60d47e9f38fc1da100b6
jgpawletko <jgpawletko@gmail.com>
2015-02-11 17:08:44 -0500

    add QueueEntry exception for bad-action files

--- lib/git_transactor/queue_entry.rb
+++ lib/git_transactor/queue_entry.rb
@@ -1,11 +1,13 @@
 module GitTransactor
   class QueueEntry
     attr_accessor :action, :path
+    ALLOWABLE_ACTIONS = %q(add rm)
     def initialize(path)
       File.open(path) do |f|
         lines = f.readlines
         raise ArgumentError.new("Too many lines in request file: #{path}") unless lines.length == 1
         @action, @path = lines[0].split(',')
+        raise ArgumentError.new("Invalid action: #{@action}") unless ALLOWABLE_ACTIONS.include?(@action)
       end
     end
   end
--- spec/git_transactor/queue_entry_spec.rb
+++ spec/git_transactor/queue_entry_spec.rb
@@ -5,6 +5,7 @@ module GitTransactor
     let(:fixture_root) { File.join('spec','fixtures','queue_entries') }
     let(:valid_entry)  { File.join(fixture_root, 'valid-entry.csv') }
     let(:multi_line_entry)  { File.join(fixture_root, 'multi-line-entry.csv') }
+    let(:bad_action_entry)  { File.join(fixture_root, 'bad-action-entry.csv') }
 
     context "when class is instantiated" do
       subject { QueueEntry.new(valid_entry) }
@@ -12,8 +13,14 @@ module GitTransactor
     end
 
     context "when given a multi-line entry file" do
-      it "should raise and ArgumentError" do
+      it "should raise an ArgumentError" do
         expect{ QueueEntry.new(multi_line_entry) }.to raise_error(ArgumentError)
+      end
+    end
+
+    context "when given a invalid action entry file" do
+      it "should raise an ArgumentError" do
+        expect{ QueueEntry.new(bad_action_entry) }.to raise_error(ArgumentError)
       end
     end
   end
