a9d39b06a43b1a9cb52ead5b5916aedd1d2382ee
jgpawletko <jgpawletko@gmail.com>
2015-02-11 17:36:39 -0500

    rename QueueEntry fixture file

--- spec/git_transactor/queue_entry_spec.rb
+++ spec/git_transactor/queue_entry_spec.rb
@@ -3,23 +3,23 @@ require 'spec_helper'
 module GitTransactor
   describe QueueEntry do
     let(:fixture_root) { File.join('spec','fixtures','queue_entries') }
-    let(:valid_entry)  { File.join(fixture_root, 'valid-entry.csv') }
+    let(:add_entry)  { File.join(fixture_root, 'add-entry.csv') }
     let(:multi_line_entry)  { File.join(fixture_root, 'multi-line-entry.csv') }
     let(:bad_action_entry)  { File.join(fixture_root, 'bad-action-entry.csv') }
     let(:bad_delimiter_entry) { File.join(fixture_root, 'bad-delimiter-entry.csv') }
     let(:multi_delimiter_entry) { File.join(fixture_root, 'multi-delimiter-entry.csv') }
 
     context "when class is instantiated" do
-      subject { QueueEntry.new(valid_entry) }
+      subject { QueueEntry.new(add_entry) }
       it { is_expected.to be_a(GitTransactor::QueueEntry) }
     end
 
     context "when given a valid entry file" do
       it "should have the correct action" do
-        expect(QueueEntry.new(valid_entry).action).to be == 'add'
+        expect(QueueEntry.new(add_entry).action).to be == 'add'
       end
       it "should have the correct path" do
-        expect(QueueEntry.new(valid_entry).path).to be == '/Users/jgp/tmp/4b3f99d8-a714-4d9c-9109-2ed5fb6d8ab4.txt'
+        expect(QueueEntry.new(add_entry).path).to be == '/Users/jgp/tmp/4b3f99d8-a714-4d9c-9109-2ed5fb6d8ab4.txt'
       end
     end
 
