9692587ba67848fc912faa617d42391736ba00a5
jgpawletko <jgpawletko@gmail.com>
2015-02-20 12:16:26 -0500

    refactor TestRepo#init to use #create_root

--- features/support/test_repo.rb
+++ features/support/test_repo.rb
@@ -2,7 +2,7 @@ class TestRepo < TestDir
   attr_reader :path
 
   def init
-    FileUtils.mkdir(@path)
+    create_root
     g = Git.init(@path)
     dummy_file = "#{@path}/foo.txt"
     File.open(dummy_file, 'w') {|f| f.puts(dummy_file)}
