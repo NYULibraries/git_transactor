78ab1477d125422f14e558b5e21ef35bbbb5ccce
jgpawletko <jgpawletko@gmail.com>
2015-02-20 12:42:07 -0500

    update TestRepo to forward to Git object

--- features/support/test_repo.rb
+++ features/support/test_repo.rb
@@ -1,12 +1,17 @@
+require 'forwardable'
+
 class TestRepo < TestDir
   attr_reader :path
 
+  extend Forwardable
+  def_delegators :@g, :add, :commit
+
   def init
     create_root
-    g = Git.init(@path)
+    @g = Git.init(@path)
     dummy_file = "#{@path}/foo.txt"
     File.open(dummy_file, 'w') {|f| f.puts(dummy_file)}
-    g.add(File.basename(dummy_file))
-    g.commit('Initial commit')
+    @g.add(File.basename(dummy_file))
+    @g.commit('Initial commit')
   end
 end
