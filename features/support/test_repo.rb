0e443388659adc96f58949bbe169d0dec064ed2d
jgpawletko <jgpawletko@gmail.com>
2015-02-24 11:14:46 -0500

    remove commits from TestRepo#init
    Move test repo file additiona and commit from TestDir#init
      to base_spec setup code.  This change enables "bare" git repos
      to be initialized in TestDir#init.

--- features/support/test_repo.rb
+++ features/support/test_repo.rb
@@ -9,10 +9,6 @@ class TestRepo < TestDir
   def init
     create_root
     @g = Git.init(@path, @options)
-    dummy_file = 'foo.txt'
-    create_file(dummy_file,dummy_file)
-    @g.add(dummy_file)
-    @g.commit('Initial commit')
   end
 
   def open
