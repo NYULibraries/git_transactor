c122a0fc9170ad623be491da745986a84ccbf825
jgpawletko <jgpawletko@gmail.com>
2015-02-24 21:45:51 -0500

    update push scenario step definitions

--- features/step_definitions/push_steps.rb
+++ features/step_definitions/push_steps.rb
@@ -1,7 +1,7 @@
 Given(/^that the remote repository exists$/) do
   @remote_repo_path = 'features/fixtures/remote_repo/blerf'
 
-  @remote_repo = TestRepo.new(@remote_repo_path)
+  @remote_repo = TestRepo.new(@remote_repo_path, bare: true)
   @remote_repo.nuke
   @remote_repo.init
 end
@@ -13,28 +13,36 @@ Given(/^that the local repository exists and was cloned from the remote reposito
   @local_repo_name = 'blerf'
   @local_repo_path = File.join(@local_repo_parent, @local_repo_name)
   Git.clone(@remote_repo.path, @local_repo_name, path: @local_repo_parent)
+  @local_repo = TestRepo.new(@local_repo_path)
+  @local_repo.open
+  local_repo_head  = Git.ls_remote(@local_repo.path)['head'][:sha]
+  remote_repo_head = Git.ls_remote(@remote_repo.path)['head'][:sha]
+  expect(local_repo_head).to be == remote_repo_head
 end
 
 Given(/^the local repository has changes that the remote repository does not$/) do
   rel_path = 'oranges/blueberries.txt'
   subdir = rel_path.split('/')[0]
 
-  @local_repo = TestRepo.new(@local_repo_path)
-  @local_repo.open
   @local_repo.create_sub_directory(subdir)
   @local_repo.create_file(rel_path, 'meaningless drivel')
   @local_repo.add(rel_path)
   @local_repo.commit("Updating file #{rel_path}")
-  match = @local_repo.status.select {|x| x.path == rel_path }
-   expect(match).not_to be_empty
-  match = @remote_repo.status.select {|x| x.path == rel_path }
-   expect(match).to be_empty
+  local_repo_head = Git.ls_remote(@local_repo.path)['head'][:sha]
+  remote_repo_head = Git.ls_remote(@remote_repo.path)['head'][:sha]
+  expect(local_repo_head).not_to be == remote_repo_head
 end
 
 When(/^I push the local repository to the remote repository$/) do
-  pending # express the regexp above with the code you wish you had
+  gt = GitTransactor::Base.new(repo_path:   @local_repo.path,
+                               source_path: 'features/fixtures/source',
+                               work_root:   'features/fixtures/work',
+                               remote_url:  @remote_repo_path )
+  gt.push
 end
 
 Then(/^the local repository and the remote repository should be in the same state$/) do
-  pending # express the regexp above with the code you wish you had
+  local_repo_head  = Git.ls_remote(@local_repo.path)['head'][:sha]
+  remote_repo_head = Git.ls_remote(@remote_repo.path)['head'][:sha]
+  expect(local_repo_head).to be == remote_repo_head
 end
