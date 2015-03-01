2612026262c3f64c91f7950321b64f45b78f245a
jgpawletko <jgpawletko@gmail.com>
2015-03-01 13:31:45 -0500

    refactor TestQueue
    add private method add_entry, add #add_failed, #add_passed to
      support QueueManager test setup

--- test_support/helpers/test_queue.rb
+++ test_support/helpers/test_queue.rb
@@ -13,10 +13,20 @@ class TestQueue < TestDir
      @passed_dir].each {|d| FileUtils.mkdir(d)}
   end
   def enqueue(action, source_path)
+    add_entry(action, source_path, @queue_dir)
+  end
+  def add_failed(action, source_path)
+    add_entry(action, source_path, @failed_dir)
+  end
+  def add_passed(action, source_path)
+    add_entry(action, source_path, @passed_dir)
+  end
+  private
+  def add_entry(action, source_path, tgt_path)
     raise ArgumentError.new("invalid action: #{action}") unless
       ['add','rm'].include?(action)
     entry = "#{Time.now.strftime("%Y%m%dT%H%M%S%9N")}.csv"
-    entry_path = File.join(@queue_dir, entry)
+    entry_path = File.join(tgt_path, entry)
     File.open(entry_path, "w") do |f|
       f.puts("#{action},#{source_path}")
     end
