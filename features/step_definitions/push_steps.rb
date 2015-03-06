c7f23407a63cc1440f6d99a478eac7d24672191d
jgpawletko <jgpawletko@gmail.com>
2015-03-06 10:43:12 -0500

    rename @local_repo to @repo
    rename variable to promote step definition reuse

--- features/step_definitions/push_steps.rb
+++ features/step_definitions/push_steps.rb
@@ -7,15 +7,15 @@ Given(/^that the remote repository exists$/) do
 end
 
 Given(/^that the local repository exists and was cloned from the remote repository$/) do
-  @local_repo_parent = 'features/fixtures/local_repo'
-  td = TestDir.new(@local_repo_parent)
+  @repo_parent = 'features/fixtures/local_repo'
+  td = TestDir.new(@repo_parent)
   td.nuke
-  @local_repo_name = 'blerf'
-  @local_repo_path = File.join(@local_repo_parent, @local_repo_name)
-  Git.clone(@remote_repo.path, @local_repo_name, path: @local_repo_parent)
-  @local_repo = TestRepo.new(@local_repo_path)
-  @local_repo.open
-  local_repo_head  = Git.ls_remote(@local_repo.path)['head'][:sha]
+  @repo_name = 'blerf'
+  @repo_path = File.join(@repo_parent, @repo_name)
+  Git.clone(@remote_repo.path, @repo_name, path: @repo_parent)
+  @repo = TestRepo.new(@repo_path)
+  @repo.open
+  local_repo_head  = Git.ls_remote(@repo.path)['head'][:sha]
   remote_repo_head = Git.ls_remote(@remote_repo.path)['head'][:sha]
   expect(local_repo_head).to be == remote_repo_head
 end
@@ -24,17 +24,17 @@ Given(/^the local repository has changes that the remote repository does not$/) 
   rel_path = 'oranges/blueberries.txt'
   subdir = rel_path.split('/')[0]
 
-  @local_repo.create_sub_directory(subdir)
-  @local_repo.create_file(rel_path, 'meaningless drivel')
-  @local_repo.add(rel_path)
-  @local_repo.commit("Updating file #{rel_path}")
-  local_repo_head = Git.ls_remote(@local_repo.path)['head'][:sha]
+  @repo.create_sub_directory(subdir)
+  @repo.create_file(rel_path, 'meaningless drivel')
+  @repo.add(rel_path)
+  @repo.commit("Updating file #{rel_path}")
+  local_repo_head = Git.ls_remote(@repo.path)['head'][:sha]
   remote_repo_head = Git.ls_remote(@remote_repo.path)['head'][:sha]
   expect(local_repo_head).not_to be == remote_repo_head
 end
 
 When(/^I push the local repository to the remote repository$/) do
-  gt = GitTransactor::Base.new(repo_path:   @local_repo.path,
+  gt = GitTransactor::Base.new(repo_path:   @repo.path,
                                source_path: 'features/fixtures/source',
                                work_root:   'features/fixtures/work',
                                remote_url:  @remote_repo_path )
@@ -42,7 +42,7 @@ When(/^I push the local repository to the remote repository$/) do
 end
 
 Then(/^the local repository and the remote repository should be in the same state$/) do
-  local_repo_head  = Git.ls_remote(@local_repo.path)['head'][:sha]
+  local_repo_head  = Git.ls_remote(@repo.path)['head'][:sha]
   remote_repo_head = Git.ls_remote(@remote_repo.path)['head'][:sha]
   expect(local_repo_head).to be == remote_repo_head
 end
