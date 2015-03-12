af907a46fb949e04294edcd744959094c4b27fb3
jgpawletko <jgpawletko@gmail.com>
2015-03-12 14:20:33 -0400

    clean up spacing
    Outdent "private", add blank lines between methods.

--- lib/git_transactor/base.rb
+++ lib/git_transactor/base.rb
@@ -42,7 +42,7 @@ module GitTransactor
       @repo.push(@remote_url)
     end
 
-    private
+private
     def process_entry(qe)
       @qe = qe
       case
@@ -52,6 +52,7 @@ module GitTransactor
         raise ArgumentError.new("unrecognized action: #{@qe.action}")
       end
     end
+
     def process_add_entry
       setup_paths
       create_repo_subdir_if_needed
@@ -59,35 +60,44 @@ module GitTransactor
       git_add_file_to_repo
       update_commit_msg_for_add_entry
     end
+
     def process_rm_entry
       setup_paths
       git_rm_file_from_repo
       update_commit_msg_for_rm_entry
     end
+
     def setup_paths
       @file_rel_path = Utils.source_path_to_repo_path(@qe.path)
       @dir_rel_path  = File.dirname(@file_rel_path)
       @dir_tgt_path  = File.join(@repo_path, @dir_rel_path)
       @file_tgt_path = File.join(@repo_path, @file_rel_path)
     end
+
     def create_repo_subdir_if_needed
       FileUtils.mkdir(@dir_tgt_path) unless File.directory?(@dir_tgt_path)
     end
+
     def copy_src_file_to_repo
       FileUtils.cp(@qe.path, @file_tgt_path, preserve: true)
     end
+
     def git_add_file_to_repo
       @repo.add(@file_rel_path)
     end
+
     def update_commit_msg_for_add_entry
       @commit_msg += (delimiter + "Updating file #{@file_rel_path}")
     end
+
     def git_rm_file_from_repo
       @repo.remove(@file_rel_path)
     end
+
     def update_commit_msg_for_rm_entry
       @commit_msg += (delimiter + "Deleting file #{@file_rel_path}")
     end
+
     def delimiter
       @commit_msg.empty? ? '' : ', '
     end
