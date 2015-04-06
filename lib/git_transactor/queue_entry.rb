dab8e810e596f8c9d624ae94209cd42cbdaa20cc
jgpawletko <jgpawletko@gmail.com>
2015-04-06 12:25:27 -0400

    add space around method definitions

--- lib/git_transactor/queue_entry.rb
+++ lib/git_transactor/queue_entry.rb
@@ -8,15 +8,18 @@ module GitTransactor
     def initialize(entry_path)
       @entry_path = entry_path
       @entry_name = File.basename(@entry_path)
+
       File.open(@entry_path) do |f|
         lines = f.readlines
         raise ArgumentError.new("Too many lines in request file: #{@entry_path}") unless lines.length == 1
         @action, @path = lines[0].chomp.split(',',2)
         raise ArgumentError.new("Invalid action: #{@action}") unless ALLOWABLE_ACTIONS.include?(@action)
       end
+
       def add?
         @action == 'add'
       end
+
       def rm?
         @action == 'rm'
       end
