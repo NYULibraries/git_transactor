bb09efceb3f7cdf28495c8ee9ec224665f626017
jgpawletko <jgpawletko@gmail.com>
2015-04-06 09:59:49 -0400

    clean up
    Add top-level module documentation comment.
    Split line to conform to max line length.

--- lib/git_transactor/utils.rb
+++ lib/git_transactor/utils.rb
@@ -1,8 +1,11 @@
 module GitTransactor
+  ##
+  # contains Git Transactor utility methods
   class Utils
     def self.source_path_to_repo_path(path)
       # current repo structure is <repo root>/<dir>/<file>
-      File.absolute_path(path).split(File::SEPARATOR)[-2..-1].join(File::SEPARATOR)
+      File.absolute_path(path).split(File::SEPARATOR)[-2..-1]
+        .join(File::SEPARATOR)
     end
   end
 end
